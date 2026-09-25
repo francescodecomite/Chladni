3add2fbc4f06a042193698cd8741c443dcb54932
Francesco de Comité <francesco.de-comite@univ-lille.fr>
2026-09-25 18:13:35 +0200

    on avance

--- potentiometre/potentiometre.ino
+++ potentiometre/potentiometre.ino
@@ -13,10 +13,12 @@ void setup() {
 void loop() {
    int analogValue = analogRead(34);
   // Rescale to potentiometer's voltage (from 0V to 3.3V):
-  int freq = ap(analogValue, 0, 4095, 100, 1500);
+  int freq = map(analogValue, 0, 4095, 100, 1500);
+  
+  Serial.println(freq);
 
-  tone(BUZZER_PIN, 440);       // Play concert A (440 Hz)
-  delay(100000);                 // Let it play for one second
-  noTone(BUZZER_PIN);          // Stop the tone
-  delay(500);                  // Pause for half a second
+  tone(BUZZER_PIN, freq);       // Play concert A (440 Hz)
+  delay(100);                 // Let it play for one second
+  //noTone(BUZZER_PIN);          // Stop the tone
+  //delay(500);                  // Pause for half a second
 }
