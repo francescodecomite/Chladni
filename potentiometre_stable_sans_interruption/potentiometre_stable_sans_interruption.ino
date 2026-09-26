eba787013ec4a95b732689863e2862e2742073cc
Francesco de Comité <francesco.de-comite@univ-lille.fr>
2026-09-26 19:29:05 +0200

    son plus stable.
    Demain, j'essaie les figures

--- potentiometre_stable_sans_interruption/potentiometre_stable_sans_interruption.ino
+++ potentiometre_stable_sans_interruption/potentiometre_stable_sans_interruption.ino
@@ -7,26 +7,36 @@ EN fait les interruptions c'est le bazar, cette version devrait marcher
  */
 #include <Arduino.h>
 int analogValue=100; 
-int newValue=analogValue; 
+int newValue=analogValue;
+int freq=440;
+const int BUZZER_PIN=26; 
 
 int n=0; 
 
 void setup() {
   
   Serial.begin(115200);
- 
+  analogValue=analogRead(34); 
+  Serial.println("ça commence...");
+  pinMode(BUZZER_PIN, OUTPUT);
+  
+
+
 }
 
 void loop() {
  n=n+1; 
  newValue=analogRead(34); 
  if (n%10000==0){
-  Serial.print(n); 
-  Serial.print(" ");
+ 
   if(abs(newValue-analogValue)>0.5*analogValue){
     analogValue=newValue; 
+    freq = map(analogValue, 0, 4095, 100, 1500);
+    Serial.println(freq);
+    tone(BUZZER_PIN, freq); 
   }
-  Serial.println(analogValue);
+ 
  }
+  
 }
 
